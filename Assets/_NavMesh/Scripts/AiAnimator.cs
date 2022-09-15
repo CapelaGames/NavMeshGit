575eede4e50fcc306f618c7eba3b9b8cb4142cb7
CapelaGames <andrew@Capelagames.com.au>
2022-09-15 14:06:46 +1000

    animations + multiai + misc

---
+++ Assets/_NavMesh/Scripts/AiAnimator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiAnimator : MonoBehaviour
+{
+    private Animator _anim;
+    private NavMeshAgent _agent;
+
+    void Start()
+    {
+        _anim = GetComponentInChildren<Animator>();
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if(_agent.velocity.magnitude > 0.1f)
+        {
+            _anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            _anim.SetBool("isWalking", false);
+        }
+    }
+}
